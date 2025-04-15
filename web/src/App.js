02ba8703d3e0a8fd6df789d9610f6c857e8f2939
“Michal <michalkoudelajunior@seznam.cz>
2025-04-15 12:53:30 +0200

    Creating code

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
 function App() {
+  let selectedOption = 0;
   return (
     <div className="App">
-      <Menu />
+      <div>
+        if (selectedOption === 0) {
+          <Home />
+        } else if (selectedOption === 1) {
+          <Article />
+        } else if (selectedOption === 2) {
+          <Book />
+        } else if (selectedOption === 3) {
+          <Quizes /> 
+        }
+      </div>
     </div>
   );
 }
@@ -11,7 +22,7 @@ function Menu(){
   return (
     <nav>
       <ul className="menu">
-        <li><h2>Domov</h2></li>
+        <li><h2 onClick>Domov</h2></li>
         <li><h2>Články</h2></li>
         <li><h2>Kniha</h2></li>
         <li><h2>Kvízy</h2></li>
@@ -20,15 +31,49 @@ function Menu(){
   );
 }
 function Article(){
-
+<>
+      <div className="menu">
+        <Menu />
+      </div>
+      <div className="content">
+        <h1 className="title">Historie IT</h1>
+        <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id nunc nec justo facilisis aliquet. Donec non odio at ligula venenatis efficitur. Sed euismod, nisi vel consectetur interdum, nisl nisi cursus enim, ac lacinia erat erat a augue.</p>
+      </div>
+   </>
 }
 function Book(){
-
+<>
+      <div className="menu">
+        <Menu />
+      </div>
+      <div className="content">
+        <h1 className="title">Historie IT</h1>
+        <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id nunc nec justo facilisis aliquet. Donec non odio at ligula venenatis efficitur. Sed euismod, nisi vel consectetur interdum, nisl nisi cursus enim, ac lacinia erat erat a augue.</p>
+      </div>
+   </>
 }
 function Quizes(){
-
+<>
+      <div className="menu">
+        <Menu />
+      </div>
+      <div className="content">
+        <h1 className="title">Historie IT</h1>
+        <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id nunc nec justo facilisis aliquet. Donec non odio at ligula venenatis efficitur. Sed euismod, nisi vel consectetur interdum, nisl nisi cursus enim, ac lacinia erat erat a augue.</p>
+      </div>
+   </>
 }
 function Home(){
-  
+  return (
+    <>
+      <div className="menu">
+        <Menu />
+      </div>
+      <div className="content">
+        <h1 className="title">Historie IT</h1>
+        <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id nunc nec justo facilisis aliquet. Donec non odio at ligula venenatis efficitur. Sed euismod, nisi vel consectetur interdum, nisl nisi cursus enim, ac lacinia erat erat a augue.</p>
+      </div>
+   </>
+  );
 }
 export default App;
